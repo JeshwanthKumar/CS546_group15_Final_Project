9ded7e08a5bcdf08b11c426ccc71ed621f67dc8c
dxiong777 <95152183+dxiong777@users.noreply.github.com>
2021-12-11 03:05:47 -0500

    added AJAX in product search
    edited landing page
    added shops in searched details
    added product list

---
+++ config/mongoConnections.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const settings =  {
+    mongoConfig: {
+      // serverUrl: 'mongodb+srv://smart:007@qwerT@cluster0.cunqm.mongodb.net/',
+      serverUrl: 'mongodb://localhost:27017/',
+      database: 'Group_15'
+    }
+  };
+  const mongoConfig = settings.mongoConfig;
+  let _connection = undefined;
+let _db = undefined;
+
+
+module.exports = {
+    getDb : async () => {
+
+  if (!_connection) {
+    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+      useNewUrlParser: true
+    }, {useUnifiedTopology: true});
+    _db = await _connection.db(mongoConfig.database);
+  }
+
+  return _db;
+
+},
+removeConnection: async () => {
+    _connection.close();
+  }
+}
+
