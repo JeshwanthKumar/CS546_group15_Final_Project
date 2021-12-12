bb621115d7d61be934b003b01dc57f6c43e004a5
Jeshwanth Kumar <jkumar3@stevens.edu>
2021-12-12 17:54:42 -0500

    edit

--- validation/routesvalidation.js
+++ validation/routesvalidation.js
@@ -49,9 +49,9 @@ const routefirstnamevalidation = (ownerFirstname) =>{
     return 'first name must be provided';
     if(ownerFirstname.trim().length ===0)
     return 'The first name contains only white spaces';
-    var regex =new RegExp(/^[A-Z]+$/);
-     if(!(regex.test(ownerFirstname)))
-     return 'The first name must contain only alphabets';
+    // var regex =new RegExp(/^[A-Z]+$/);
+    //  if((regex.test(ownerFirstname)))
+    //  return 'The first name must contain only alphabets';
 }
 
 const routelastnamevalidation = (ownerLastname) =>{
@@ -63,9 +63,9 @@ const routelastnamevalidation = (ownerLastname) =>{
     return 'username must be provided';
     if(ownerLastname.trim().length ===0)
     return 'Thelast name contains only white spaces';
-    var regex =new RegExp(/^[A-Z]+$/);
-    if(!(regex.test(ownerLastname)))
-    return 'The last name must contain only alphabets ';
+    // var regex =new RegExp(/^[A-Z]+$/);
+    // if(!(regex.test(ownerLastname)))
+    // return 'The last name must contain only alphabets ';
 }
 
 const routeemailvalidation = (email) =>{
@@ -77,9 +77,9 @@ const routeemailvalidation = (email) =>{
     return 'email must be provided';
     if(email.trim().length ===0)
     return 'The email contains only white spaces';
-    var regex =new RegExp(/^[a-zA-Z0-9.!#$%&'*+\=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
-    if(!(regex.test(email)))
-    return 'The email must be in the correct format';
+    // var regex =new RegExp(/^[a-zA-Z0-9.!#$%&'*+\=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
+    // if(!(regex.test(email)))
+    // return 'The email must be in the correct format';
 }
 
 const routepincodevalidation = (pincode) =>{
@@ -91,9 +91,9 @@ const routepincodevalidation = (pincode) =>{
     return 'pincode must be provided';
     if(pincode.trim().length ===0)
     return 'The pincode contains only white spaces';
-    var regex =new RegExp(/^[0-9]{5}(?:-[0-9]{4})?$/);
-    if(!(regex.test(pincode)))
-    return 'The pincode must be in the correct format';
+    // var regex =new RegExp(/^[0-9]{5}(?:-[0-9]{4})?$/);
+    // if(!(regex.test(pincode)))
+    // return 'The pincode must be in the correct format';
 }
 
 const routephonenumbervalidation = (phoneNumber) =>{
@@ -103,9 +103,9 @@ const routephonenumbervalidation = (phoneNumber) =>{
     return 'phone number must be provided';
     if(phoneNumber.trim().length ===0)
     return 'The phone number contains only white spaces';
-    var regex =new RegExp(/^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/);
-    if(!(regex.test(phoneNumber)))
-    return 'The phone number must be in the correct format';
+    // var regex =new RegExp(/^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/);
+    // if(!(regex.test(phoneNumber)))
+    // return 'The phone number must be in the correct format';
 }
 
 
