f2ddf393add8c0fe8ff8f06f328a6f107a8eb798
theHparth <91298053+theHparth@users.noreply.github.com>
2021-12-13 17:39:25 -0500

    change config folder

--- config/mongoConnections.js
+++ config/mongoConnections.js
@@ -1,34 +1,3 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const settings =  {
-//     mongoConfig: {
-//       // serverUrl: 'mongodb+srv://smart:007@qwerT@cluster0.cunqm.mongodb.net/',
-//       // database: 'Group_15'
-//      //     serverUrl: 'mongodb://localhost:27017/',
-//     database: 'G15'
-//     }
-//   };
-//   const mongoConfig = settings.mongoConfig;
-//   let _connection = undefined;
-// let _db = undefined;
-
-
-// module.exports = {
-//     getDb : async () => {
-
-//   if (!_connection) {
-//     _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-//       useNewUrlParser: true
-//     }, {useUnifiedTopology: true}, { useUnifiedTopology: true });
-//     _db = await _connection.db(mongoConfig.database);
-//   }
-
-//   return _db;
-
-// },
-// removeConnection: async () => {
-//     _connection.close();
-//   }
-// }
 
 const MongoClient = require('mongodb').MongoClient;
 const settings = {
@@ -56,5 +25,3 @@ module.exports = async () => {
 
   return _db;
 };
-
-
