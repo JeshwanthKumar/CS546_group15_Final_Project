b6a4ce01d375ac1657ecc9665155b36d06420e57
theHparth <91298053+theHparth@users.noreply.github.com>
2021-12-13 01:54:55 -0500

    live databse

--- config/mongoConnections.js
+++ config/mongoConnections.js
@@ -1,60 +1,30 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const settings =  {
-//     mongoConfig: {
-//       // serverUrl: 'mongodb+srv://smart:007@qwerT@cluster0.cunqm.mongodb.net/',
-//       // database: 'Group_15'
-//      //     serverUrl: 'mongodb://localhost:27017/',
-//     database: 'G15'
-//     }
-//   };
-//   const mongoConfig = settings.mongoConfig;
-//   let _connection = undefined;
-// let _db = undefined;
-
-
-// module.exports = {
-//     getDb : async () => {
-
-//   if (!_connection) {
-//     _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-//       useNewUrlParser: true
-//     }, {useUnifiedTopology: true}, { useUnifiedTopology: true });
-//     _db = await _connection.db(mongoConfig.database);
-//   }
-
-//   return _db;
-
-// },
-// removeConnection: async () => {
-//     _connection.close();
-//   }
-// }
-
 const MongoClient = require('mongodb').MongoClient;
-const settings = {
-  mongoConfig: {
+const settings =  {
+    mongoConfig: {
       serverUrl: 'mongodb+srv://smart:007@qwerT@cluster0.cunqm.mongodb.net/',
       database: 'Group_15'
     }
-    //serverUrl: 'mongodb://localhost:27017/',
-    //database: 'GROUP15'
-// }
-};
-const mongoConfig = settings.mongoConfig;
-
-let _connection = undefined;
+  };
+  const mongoConfig = settings.mongoConfig;
+  let _connection = undefined;
 let _db = undefined;
 
-module.exports = async () => {
+
+module.exports = {
+    getDb : async () => {
+
   if (!_connection) {
     _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+      useNewUrlParser: true
+    }, {useUnifiedTopology: true});
     _db = await _connection.db(mongoConfig.database);
   }
 
   return _db;
-};
 
+},
+removeConnection: async () => {
+    _connection.close();
+  }
+}
 
